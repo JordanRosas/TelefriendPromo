879e9b34e833a0dd4ada95cc2b52ae46ede53967
Jordan Rosas <jordanrosas1203@gmail.com>
2020-06-26 10:43:18 -0500

    removed unused import

--- src/shared/navbar/index.js
+++ src/shared/navbar/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route } from "react-router-dom" 
+import { Link } from "react-router-dom" 
 import styles from './style'
 import newIcon from '../../assets/EyeconTrans.png'
 class NavigationBar extends Component{

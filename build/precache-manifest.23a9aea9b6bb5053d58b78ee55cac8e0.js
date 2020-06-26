423c2f3912859c671e9f4e8cee56510a4b56273c
Jordan Rosas <jordanrosas1203@gmail.com>
2020-06-26 10:31:40 -0500

    added build folder

---
+++ build/precache-manifest.23a9aea9b6bb5053d58b78ee55cac8e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1f16bc3cfef72dd2b10604f47c48a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "274cd4fbebe0d2a0fe2d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbd5d882aaa3887137bc",
+    "url": "/static/js/2.7b7f9d24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7b7f9d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274cd4fbebe0d2a0fe2d",
+    "url": "/static/js/main.a26c61eb.chunk.js"
+  },
+  {
+    "revision": "9492106365da120cf580",
+    "url": "/static/js/runtime-main.5e424399.js"
+  },
+  {
+    "revision": "cef0e778bd6670e06a4cf745a504e80b",
+    "url": "/static/media/EyeconTrans.cef0e778.png"
+  },
+  {
+    "revision": "4ed038e0fb4d0848971c6a0dde65048b",
+    "url": "/static/media/IphoneImage.4ed038e0.svg"
+  },
+  {
+    "revision": "c1fb96655c38ee629909692a9c62e16d",
+    "url": "/static/media/demoicon.c1fb9665.png"
+  },
+  {
+    "revision": "c618467827d778fe46cfc744ac30c2d6",
+    "url": "/static/media/telefriendtransparent.c6184678.png"
+  },
+  {
+    "revision": "715360290f58e79ac7614a3c8b7cd6db",
+    "url": "/static/media/tf_logo.71536029.svg"
+  },
+  {
+    "revision": "6bb187c6960e66b01e7fa608b7596dc0",
+    "url": "/static/media/tfabtsquig.6bb187c6.svg"
+  },
+  {
+    "revision": "a99621604a57bd479dc2822a8119ab63",
+    "url": "/static/media/wendy.a9962160.jpg"
+  }
+]);
